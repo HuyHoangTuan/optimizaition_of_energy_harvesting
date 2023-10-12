ae3f7fa315cb204eb1488b7026e3f95910178c76
Cong Tran <congtraninf@gmail.com>
2023-10-12 21:33:12 +0700

    Revert "minor change"
    This reverts commit da6b119727420c171f6a9c4ebcc47166c3ee7a6a.

--- main.py
+++
@@ -1,9 +0,0 @@
-from Environment import Environment
-from DQN import DQN
-import torch
-
-
-if __name__ == '__main__':
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    env = Environment()
-    model = DQN(env.num_state_space() , env.num_action_space()).to(device = device)
