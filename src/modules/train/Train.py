951cc9df3ef4e1d7a1a5db5dbbaa047b8b0f9f95
HuyHt <kubin2712@gmail.com>
2024-04-26 15:23:55 +0700

    Update Train.py

--- src/modules/train/Train.py
+++ src/modules/train/Train.py
@@ -350,11 +350,11 @@ class Train:
                 # if self.steps_done % 12000 == 0:
                 #     self.target_net.load_state_dict(self.policy_net.state_dict())
 
-                dict = {}
+                _dict = {}
                 for key in self.policy_net.state_dict():
-                    dict[key] = self.policy_net.state_dict()[key] * self.tau + self.target_net.state_dict()[key] * (
+                    _dict[key] = self.policy_net.state_dict()[key] * self.tau + self.target_net.state_dict()[key] * (
                             1 - self.tau)
-                self.target_net.load_state_dict(dict)
+                self.target_net.load_state_dict(_dict)
 
 
                 # LogUtils.info(
