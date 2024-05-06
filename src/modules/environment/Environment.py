b6f3a620561611647219625ef044496eaac5580c
huyht <huyht5@vng.com.vn>
2024-05-06 17:44:03 +0700

    update env

--- src/modules/environment/Environment.py
+++ src/modules/environment/Environment.py
@@ -6,7 +6,7 @@ from utils import LogUtils, RandomUtils
 class Environment:
     def __init__(
             self,
-            NumSU = 1,
+            NumSU = 8,
             NumPU = 2,
             P_max = 1,
             Xi_s = 0.1,
@@ -243,6 +243,7 @@ class Environment:
         # return: state, action, reward, time_slot
         states = []
         Rs = 0
+        Gss = 0
 
         self._Time_Slot += 1
         v = self._get_v(self._Time_Slot)
@@ -314,6 +315,7 @@ class Environment:
             self._add_record(SU, record)
             states.append(state)
             Rs += R
+            Gss += G_s
 
         state = tuple(chain(*tuple(states)))
 
@@ -325,6 +327,7 @@ class Environment:
 
         reward = (
             Rs / self.NumSU,
+            Gss / self.NumSU,
             0
         )
 
